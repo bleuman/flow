5eb490161959825b9a5dfbc2f38a42e627441513
Your Name <you@example.com>
2018-12-30 18:15:34 +0100

    commit  on du/2

---
+++ flow-start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m' 
+blu=$'\e[1;34m'
+mag=$'\e[1;35m' 
+cyn=$'\e[1;36m' 
+end=$'\e[0m'
+
+
+echo -e "${yel}>flow.log with params $1 ${end}"
+
+case $1 in
+ a)git log --all --oneline --graph;;
+ o)git log --graph --oneline;;
+ b)git log --graph --oneline --first-parent master..;;
+ g)git log --graph --oneline;;
+ *)git log --oneline;;
+esac
