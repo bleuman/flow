1b3bca24113e85e91c473a6a2d4fdd4d2f1087f9
mbouybaoun <bouybaoun@gmail.com>
2018-12-31 11:52:37 +0100

    finish and finishp functions

--- flow.sh
+++ flow.sh
@@ -11,6 +11,32 @@ print(){
 echo -e "${yel}>flow :${end} ${mag}$1${end}"
 }
 print "runing with param $1 $2 $3 $4 $5 $6 $7 $8"
+if ["$1" == "" ]
+then
+	echo "
+	r) flow-rebase.sh $2 $3;;
+	f) git fetch;;
+	pl) git pull;;
+	ps) git push;;
+	m) git mergetool --tool=vimdiff;;
+	g) git tag -ln $2;;
+	a) print "git add --all"
+	   git add --all;;
+	b) print "git branch $2 $3 $4"
+	   git branch $2 $3 $4;;
+	s) print "git status"
+	   git status ;;
+	l) flow-log.sh $2;;
+	c) git add --all
+	   git commit -m "commit $2 on `git branch | grep \* | cut -d ' ' -f2`";;
+	k) flow-checkout.sh $2 $3 $4;;
+	st)	flow-start.sh $2 $3 $4 $5 ;;
+	fi)	flow-finish.sh $2 $3 $4 $5 ;;
+	fp)	flow-finishp.sh $2 $3 $4 $5 ;;
+	p)	print "$2 $3 $4 $5 $6 $7 $8 $9" ;;
+	"
+	exit
+fi
 case $1 in
 	r) flow-rebase.sh $2 $3;;
 	f) git fetch;;
