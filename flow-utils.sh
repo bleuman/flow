0686a8b2c05f29f2f185d071b291b53136d89eb4
Mohamed Bouybaoun <bouybaoun@gmail.com>
2019-01-02 11:22:33 +0100

    commit  on master

---
+++ flow-utils.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m' 
+blu=$'\e[1;34m'
+mag=$'\e[1;35m' 
+cyn=$'\e[1;36m' 
+end=$'\e[0m'
+
+
+getCurrentBranch(){
+	echo `git branch | grep \* | cut -d ' ' -f2`
+}
+isTagExist(){
+	echo `git tag -l "$1" | wc -l`
+}
+isTikcetExist(){
+	echo `git tag -l "s-$1" | wc -l`
+}
+
+pullAll(){
+	git pull --all
+}
+pushAll(){
+	git push --all --tags origin
+}
